cdd48d0d2f7a5490375110271973b396c4037c81
Raluca Alexandra Caltais <raluca.a.caltais@accenture.com>
2022-08-30 00:32:53 +0300

    Commit 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/') 
 #Later on here we can paste the URL of our Route
 def hello():
-    return "Hello worls!"
+    return "Hello world!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8000')
